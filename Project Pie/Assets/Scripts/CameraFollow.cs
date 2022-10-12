8f46184b5b25f1c49cdd6d6cea69c8d4eccd90c7
PhantomaNexus <jacko.tread@hotmail.com>
2022-10-12 12:49:44 +0100

    New Camera Added
    New camera system added, camera smoothly follows the player.

---
+++ Project Pie/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.325f;
+
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        // Camera Targeting
+        Vector3 desiredPosition = target.position + offset;
+        // Smoothed Camera via Lerp
+        Vector3 smoothed = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = target.position + offset;
+
+        transform.LookAt(target);
+    }
+
+
+}
